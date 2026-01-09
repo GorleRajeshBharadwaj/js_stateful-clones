0a384098374ed92b8187116aecb2933c8b817a89
RajeshBharadwaj <32881036+GorleRajeshBharadwaj@users.noreply.github.com>
2026-01-09 11:29:06 +0530

    Update transformStateWithClones.js

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,40 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  const finalArr = [];
+  const finalState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(finalState, action.extraData);
+        break;
+
+      case 'clear':
+        clearState(finalState);
+        break;
+
+      case 'removeProperties':
+        clearProperties(finalState, action.keysToRemove);
+        break;
+    }
+
+    finalArr.push({ ... finalState });
+  }
+
+  return finalArr;
+}
+
+function clearState(state) {
+  for (const key in state) {
+    delete state[key];
+  }
+}
+
+function clearProperties(state, properties) {
+  for (const key of properties) {
+    delete state[key];
+  }
 }
 
 module.exports = transformStateWithClones;
